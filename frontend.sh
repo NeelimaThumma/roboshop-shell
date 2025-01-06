cf70fe1f643bc4755236a70d4d52221e84a892b9
neeli <neelimareddygopu1@gmail.com>
2025-01-06 12:40:16 +0530

    Roboshop frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,16 @@
-dnf install nginx -y
+echo -e "\e[33m Installing the webserver \e[0m"
+dnf install nginx -y &>>/tmp/roboshop.log
 
+echo -e "\e[33m Removing the previous directories\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m Pulling the source code\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[33m Extracting the code\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[33m Enabling the services\e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
