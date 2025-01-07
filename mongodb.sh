11b4561610c22a97e54497de4878346da416022c
neeli <neelimareddygopu1@gmail.com>
2025-01-07 12:08:05 +0530

    Roboshop mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,7 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33m Installing the webserver \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongodb.conf
 echo -e "\e[33m Start the services \e[0m"
 # update the conf file /etc/mongod.conf
 systemctl enable mongod &>>/tmp/roboshop.log
